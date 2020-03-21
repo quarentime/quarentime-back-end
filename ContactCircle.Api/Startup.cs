ec2614bfb025dfab3aa1f4c0dcee195a63cc8e3f
Edmar <macbook@MacBook-Pro-Edmar.hub>
2020-03-22 00:42:30 +1300

    Json configuration

--- ContactCircle.Api/Startup.cs
+++ ContactCircle.Api/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace ContactCircle.Api
 {
@@ -25,7 +20,17 @@ namespace ContactCircle.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                    .AddNewtonsoftJson(options =>
+                    {
+                        options.SerializerSettings.ContractResolver = new DefaultContractResolver
+                        {
+                            NamingStrategy = new SnakeCaseNamingStrategy(),
+                        }; 
+                        options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter(new SnakeCaseNamingStrategy())); 
+                        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore; 
+                        options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ User.Api/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using User.Api.Model;
+
+namespace User.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        [HttpPost]
+        public async Task<UserModel> Post(UserModel value)
+        {
+            return await Task.FromResult(value);
+        }
+    }
+}
