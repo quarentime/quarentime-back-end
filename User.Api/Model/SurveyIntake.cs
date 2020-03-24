d16b535a3227666f1078e2efcc1034e2ec6b95a2
Vlatko Zmejkoski <vlatko.zmejkoski@it-labs.com>
2020-03-25 00:53:29 +0100

    Add additional survey intake properties

--- User.Api/Model/SurveyIntake.cs
+++ User.Api/Model/SurveyIntake.cs
@@ -1,13 +1,17 @@
 using Google.Cloud.Firestore;
+using System.Collections.Generic;
 
 namespace User.Api.Model
 {
     [FirestoreData]
     public class SurveyIntake
     {
-        [FirestoreProperty] public bool HasRecentTravel { get; set; }
+        [FirestoreProperty] public bool HasRecentTravelLast14Days { get; set; }
+        [FirestoreProperty] public bool HasRecentTravelBeforeSymptoms { get; set; }
         [FirestoreProperty] public bool HasCloseContact { get; set; }
         [FirestoreProperty] public bool HasSymptoms { get; set; }
         [FirestoreProperty] public bool HasRecovered { get; set; }
+        [FirestoreProperty] public bool IsTestedPositive { get; set; }
+        [FirestoreProperty] public List<Symptom> Symptoms { get; set; }
     }
 }
