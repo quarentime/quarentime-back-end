347d25f749334b4ac540301cc15f721b30ec9bdc
Vlatko Zmejkoski <vlatko.zmejkoski@it-labs.com>
2020-04-01 12:06:37 +0200

    Merge develop -> notification_api

---
+++ Notification.Api/Contracts/MessageContract.cs
@@ -0,0 +1,9 @@
+﻿namespace Notification.Api.Models
+{
+    public class MessageContract
+    {
+        public string Message { get; set; }
+        public string UserPhoneNumber { get; set; }
+        public string UserId { get; set; }
+    }
+}
--- Notification.Api/Contracts/SmsAlertContract.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Notification.Api.Models
-{
-    public class SmsAlertContract
-    {
-        public string ContactName { get; set; }
-        public string ContactStatus { get; set; }
-        public string UserPhoneNumber { get; set; }
-    }
-}
