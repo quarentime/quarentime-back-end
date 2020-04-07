b1b58df6335480f7d22b1a5e3d7444dbfdf88de9
Vlatko Zmejkoski <vlatko.zmejkoski@it-labs.com>
2020-04-07 20:07:34 +0200

    Add response logs filter

---
+++ User.Api/Filters/ResponseFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace User.Api.Filters
+{
+    public class ResponseFilter : IResultFilter
+    {
+        private readonly ILogger<ResponseFilter> _logger;
+        
+        public ResponseFilter(ILoggerFactory factory)
+        {
+            _logger = factory.CreateLogger<ResponseFilter>();
+        }
+
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            var response = context.Result as JsonResult;
+
+            _logger.LogInformation($"Response: {JsonConvert.SerializeObject(response)}");
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            
+        }
+    }
+}
